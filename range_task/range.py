8e9b02335d93cbc5e6db26326b381d6a1c203436
ainesunny <ainesunny@gmail.com>
2026-07-21 00:22:39 +0700

    range

--- range_task/range.py
+++ range_task/range.py
@@ -1,26 +1,45 @@
 class Range:
-    def __init__(self, start, end):
+    def __init__(self, start: int, end: int) -> None:
         self.__start = start
         self.__end = end
 
     @property
-    def start(self):
+    def start(self) -> int:
         return self.__start
 
     @start.setter
-    def start(self, start):
+    def start(self, start: int) -> None:
         self.__start = start
 
     @property
-    def end(self):
+    def end(self) -> int:
         return self.__end
 
     @end.setter
-    def end(self, end):
+    def end(self, end: int) -> None:
         self.__end = end
 
-    def get_length(self):
+    def get_length(self) -> int:
         return self.__end - self.__start
 
-    def is_inside(self, number):
+    def is_inside(self, number: int) -> bool:
         return self.__start <= number <= self.__end
+
+    def intersection(self, second_range: Range) -> Range | None:
+        intersection_start = max(self.__start, second_range.start)
+        intersection_end = min(self.__end, second_range.end)
+        return Range(intersection_start, intersection_end) if intersection_start < intersection_end else None
+
+    def union(self, second_range: Range) -> list[Range]:
+        if self.__start <= second_range.start:
+            left_range, right_range = self, second_range
+        else:
+            left_range, right_range = second_range, self
+
+        if left_range.__end >= right_range.start:
+            return [Range(left_range.__start, max(left_range.__end, right_range.__end))]
+        else:
+            return [Range(left_range.__start, left_range.__end), Range(right_range.start, right_range.__end)]
+
+    def __str__(self) -> str:
+        return f"[{self.__start}, {self.__end}]"
--- range_task/range_program.py
+++ range_task/range_program.py
@@ -1,12 +1,31 @@
 from range import Range
 
-user_range = Range(int(input("Введите начало диапазона: ")), int(input("Введите конец диапазона: ")))
+user_range_1 = Range(int(input("Введите начало первого диапазона: ")), int(input("Введите конец первого диапазона: ")))
 
-print(f"Вы ввели диапазон [{user_range.start}; {user_range.end}] длиной {user_range.get_length()}")
+print(f"Вы ввели первый диапазон {str(user_range_1)} длиной {user_range_1.get_length()}")
 
-user_number = float(input("Введите число: "))
+user_range_2 = Range(int(input("Введите начало второго диапазона: ")), int(input("Введите конец второго диапазона: ")))
 
-if user_range.is_inside(user_number):
-    print(f"Введенное число {user_number} входит в диапазон [{user_range.start}; {user_range.end}]")
+print(f"Вы ввели второй диапазон {str(user_range_2)} длиной {user_range_2.get_length()}")
+
+user_number = int(input("Введите число: "))
+
+if user_range_1.is_inside(user_number) and user_range_2.is_inside(user_number):
+    print(f"Введенное число {user_number} входит в оба диапазона")
+elif user_range_1.is_inside(user_number):
+    print(f"Введенное число {user_number} входит только в первый диапазон {str(user_range_1)}")
+elif user_range_2.is_inside(user_number):
+    print(f"Введенное число {user_number} входит только во второй диапазон {str(user_range_2)}")
 else:
-    print(f"Введенное число {user_number} не входит в диапазон [{user_range.start}; {user_range.end}]")
+    print(f"Введенное число {user_number} не входит ни в один из двух диапазонов")
+
+intersection_range = user_range_1.intersection(user_range_2)
+
+if intersection_range is not None:
+    print(f"Интервал пересечения двух указанных диапазонов {str(intersection_range)}")
+else:
+    print("У указанных диапазонов нет пересечения")
+
+union_range = user_range_1.union(user_range_2)
+
+print(f"Объединенный интервал из двух указанных диапазонов {[str(element) for element in union_range]}")
